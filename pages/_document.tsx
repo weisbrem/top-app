a9954ec7d07d92744ac8a8f244c15235c57dd66f
rook-a <zhernovkov_konst@mail.ru>
2022-06-24 09:30:45 +0300

    feat: подключит шрифт, подготовит переменные в css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,14 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <html lang='ru'>
-        <Head />
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' crossOrigin='anonymous' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
