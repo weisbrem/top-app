77ac0975860fe01466eb461debe76be25ec6f3c9
weisbrem <zhernovkov_konst@mail.ru>
2023-06-19 06:26:45 +0200

    feat: add basic sidebar layout

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -4,12 +4,16 @@ import { Menu } from '../Menu/Menu';
 
 import { SidebarProps } from './Sidebar.props';
 
+import Logo from '../logo.svg';
+
 import styles from './Sidebar.module.css';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
     <>
-      <div {...props}>
+      <div className={cn(styles.sidebar, className)} {...props}>
+        <Logo className={styles.logo} />
+        <div>Поиск</div>
         <Menu />
       </div>
     </>
