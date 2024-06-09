468b5de319c51805741a25b25b065b8d15886dd3
weisbrem <zhernovkov_konst@mail.ru>
2024-06-09 22:32:38 +0200

    feat: update page layout component

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import { ITopPageComponentProps } from './TopPageComponent.props';
+import { HhData, Htag, Tag } from '../../components';
+
+import styles from './TopPageComponent.module.css';
+import { TopLevelCategory } from '../../interfaces/page.interface';
 
 export function TopPageComponent({ page, products, firstCategory }: ITopPageComponentProps): JSX.Element {
-  return <>{products && products.length}</>;
+  return (
+    <main className={styles.container}>
+      <header className={styles.title}>
+        <Htag tag='h1'>{page.title}</Htag>
+        {products && (
+          <Tag color='gray' size='m'>
+            {products.length}
+          </Tag>
+        )}
+
+        <span>сортировка</span>
+      </header>
+      <div>{products && products.map((p) => <div key={p._id}>{p.title}</div>)}</div>
+
+      <div className={styles.hhTitle}>
+        <Htag tag='h2'>Вакансия — {page.category}</Htag>
+        <Tag color='red' size='m'>
+          hh.ru
+        </Tag>
+      </div>
+
+      {firstCategory === TopLevelCategory.Courses && <HhData {...page.hh} />}
+    </main>
+  );
 }
